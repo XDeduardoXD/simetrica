371e717a94090a7755e91087e8baedef15b92fde
Ale <alexmoravela@gmail.com>
2023-10-18 10:13:39 -0600

    Se añaden nuevas pruebas.

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once 'tests/SumaTest.php';
 
 echo "<strong>Resultados de las pruebas:</strong><br>";
 echo testSumar();
-// Si añades más pruebas en el futuro, simplemente llámalas aquí y muestra los resultados.
+// Se añadieron pruebas
